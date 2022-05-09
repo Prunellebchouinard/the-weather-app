9b1f463198fcf545e2330fb410dc87b4cc803fb9
Prunellebchouinard <100494090+Prunellebchouinard@users.noreply.github.com>
2022-05-09 19:33:23 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -40,7 +40,17 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "9663346d9c8350ab240d4b30ca8087ba";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "9663346d9c8350ab240d4b30ca8087ba";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);

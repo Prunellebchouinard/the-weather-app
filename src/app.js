c9c021757cd5ac1f1b83c47476c69e244797ecab
Prunellebchouinard <100494090+Prunellebchouinard@users.noreply.github.com>
2022-05-13 14:04:07 -0400

    removed extra closing div

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ function displayForecast() {
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
+
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -36,8 +37,7 @@ function displayForecast() {
                   <span class="weather-forecast-temperature-max">18°</span>
                   <span class="weather-forecast-temperature-min">12°</span>
                 </div>
-              </div>
-            </div>`;
+                </div>`;
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
